854709da52fecf20a21dde1f8aaf8193a3b4109f
kouki223 <kouki.1818194@gmail.com>
2025-06-24 19:23:00 +0900

    デプロイでエラーが発生したため一度

--- app/env.ts
+++ app/env.ts
@@ -1,8 +1,5 @@
-//APIキーを使ってAPIを叩く
-
 if (!process.env.CAT_API_KEY) {
-    throw new Error("環境変数 CAT_API_KEY が設定されていません。");
+  throw new Error("環境変数 CAT_API_KEY が設定されていません");
 }
-
-//process.envは環境変数を保存するためのもの
+ 
 export const CAT_API_KEY = process.env.CAT_API_KEY;
